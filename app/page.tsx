7af03ec1675061c1ef4f93508f18cb597996edfb
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2025-02-14 12:56:36 +0600

    remove: footer from the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
-import Header from "@/components/header"
-import Hero from "@/components/hero"
-import ProductGrid from "@/components/product-grid"
-import Footer from "@/components/footer"
+import Header from '@/components/header'
+import Hero from '@/components/hero'
+import ProductGrid from '@/components/product-grid'
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className='flex flex-col min-h-screen'>
       <Header />
-      <main className="flex-grow">
+      <main className='flex-grow'>
         <Hero />
         <ProductGrid />
       </main>
-      <Footer />
     </div>
   )
 }
-
